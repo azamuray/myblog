2fe3c9cd054fec46b80f74c4ec88ab897fceb58e
ai-rage <azamat.murdalov@mail.ru>
2018-11-30 11:24:57 +0300

    add home and about templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+posts = [
+	{
+		'author': 'Азамат',
+		'title': 'Первый пост',
+		'content': 'Контент первого поста',
+		'date_posted': 'Август 22, 2018'
+	},
+	{
+		'author': 'Морфеус',
+		'title': 'Две таблетки',
+		'content': 'Ты как Алиса в крольичей норе',
+		'date_posted': 'Август 28, 2018'
+	}
+]
+
+
+def home(request):
+	context = {
+		'posts': posts
+	}
+	return render(request, 'blog/home.html', context)
+
+def about(request):
+	return render(request, 'blog/about.html', {'title': 'Обо мне'})
