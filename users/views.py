b6bce6e73d7e08137e68e92b7b98a0c491626f3d
ai-rage <azamat.murdalov@mail.ru>
2018-12-01 23:37:58 +0300

    update register

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from .forms import UserRegisterForm
 
 def register(request):
@@ -6,6 +7,8 @@ def register(request):
 		form = UserRegisterForm(request.POST)
 		if form.is_valid():
 			form.save()
+			username = form.cleaned_data.get('username')
+			messages.success(request, f'Аккаунт {username} успешно создан!')
 			return redirect('blog-home')
 	else:
 		form = UserRegisterForm()
