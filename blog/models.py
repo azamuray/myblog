184ed57430b4d6fe7cb84d90a37398dc9dc5cbfb
ai-rage <azamat.murdalov@mail.ru>
2018-12-03 22:31:48 +0300

    add post create view

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Post(models.Model):
-	title = models.CharField(max_length=120)
-	content = models.TextField()
-	date_posted = models.DateTimeField(default=timezone.now)
+	title = models.CharField('Заголовок', max_length=120)
+	content = models.TextField('Описание')
+	date_posted = models.DateTimeField('Дата публикации', default=timezone.now)
 	author = models.ForeignKey(User, on_delete=models.CASCADE)
 
 	def __str__(self):
 		return self.title
+
+	def get_absolute_url(self):
+		return reverse('post-detail', kwargs={'pk': self.pk})
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import (
+	ListView,
+	DetailView,
+	CreateView
+)
 from .models import Post
 
 
@@ -9,8 +14,19 @@ class PostListView(ListView):
 	context_object_name = 'posts'
 	ordefing = ['-date_posted']
 
+
 class PostDetailView(DetailView):
 	model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+	model = Post
+	fields = ['title', 'content']
+
+	def form_valid(self, form):
+		form.instance.author = self.request.user
+		return super().form_valid(form)
+
 
 def about(request):
 	return render(request, 'blog/about.html', {'title': 'Обо мне'})
