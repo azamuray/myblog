c9ae94a33cb140f1192b1238cab1e6ebaa006d24
ai-rage <azamat.murdalov@mail.ru>
2018-12-03 21:31:25 +0300

    add post detail view

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
-def home(request):
-	context = {
-		'posts': Post.objects.all()
-	}
-	return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+	model = Post
+	template_name = 'blog/home.html'
+	context_object_name = 'posts'
+	ordefing = ['-date_posted']
+
+class PostDetailView(DetailView):
+	model = Post
 
 def about(request):
 	return render(request, 'blog/about.html', {'title': 'Обо мне'})
