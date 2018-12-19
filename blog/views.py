c184aedc908adf25e111d2ced351901fddc05234
ai-rage <azamat.murdalov@mail.ru>
2018-12-19 21:23:16 +0300

    add order posts by user

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 from django.views.generic import (
 	ListView,
 	DetailView,
@@ -17,8 +18,19 @@ class PostListView(ListView):
 	paginate_by = 5
 
 
+class UserPostListView(ListView):
+	model = Post
+	template_name = 'blog/user_posts.html'
+	context_object_name = 'posts'
+	paginate_by = 5
+
+	def get_queryset(self):
+		user = get_object_or_404(User, username=self.kwargs.get('username'))
+		return Post.objects.filter(author=user).order_by('-date_posted')
+
 class PostDetailView(DetailView):
 	model = Post
+	# view: <app>/<model>_<viewtype>.html
 
 
 class PostCreateView(LoginRequiredMixin, CreateView):
